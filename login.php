33111ac737e1822d34880547a62560d66c9ba44a
Anthony-Terravecchia <70961223+Anthony-Terravecchia@users.noreply.github.com>
2024-02-26 16:10:35 -0500

    anthony: basic login

--- login.php
+++ login.php
@@ -2,24 +2,31 @@
 
 $email = $_POST['email'];
 $password = $_POST['password'];
- 
-$myconnection = mysqli_connect('localhost', 'root', '') 
-   or die ('Could not connect: ' . mysql_error());
 
-$mydb = mysqli_select_db ($myconnection, 'db2') or die ('Could not select database');
+// Establish connection to your database
+$myconnection = mysqli_connect('localhost', 'root', '') or die ('Could not connect: ' . mysqli_error());
 
-$query = 'SELECT email FROM account WHERE email = ' . $email;
-$result = mysqli_query($myconnection, $query) or die("Query Failed: " . mysql_error());
-if($result = NULL) {
-  echo 'No matching email';
+$mydb = mysqli_select_db($myconnection, 'db2') or die ('Could not select database');
+
+$query = "SELECT email, password FROM account WHERE email = '$email'";
+$result = mysqli_query($myconnection, $query) or die("Query Failed: " . mysqli_error($myconnection));
+
+if(mysqli_num_rows($result) == 0) {
+    echo 'No matching email';
+} else {
+    $row = mysqli_fetch_assoc($result);
+    if($row['password'] != $password) {
+        echo 'Incorrect Password';
+    } else {
+        echo 'Login successful';
+        // Here you can redirect the user to another page or perform other actions upon successful login
+    }
 }
-else
-{
-  $query = 'SELECT password FROM account WHERE email = ' . $email;
-  $result = mysqli_query($mydb,$query) or die("Query Failed: " . mysql_error());
-  if($resut != $password)
-  {
-    echo 'Incorrect Password';
-  }
-}
- ?>
+
+// Free result set
+mysqli_free_result($result);
+
+// Close connection
+mysqli_close($myconnection);
+
+?>
