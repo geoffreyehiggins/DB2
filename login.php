478b2097d9f62e5b9cccafe00697bdb8473b08df
geoffreyehiggins <geoffrey_higgins@student.uml.edu>
2024-02-26 15:10:45 -0500

    update

--- login.php
+++ login.php
@@ -6,6 +6,6 @@ $password = $_POST['password']
 $myconnection = mysqli_connect('localhost', 'root', '') 
    or die ('Could not connect: ' . mysql_error());
 
- $mydb = mysqli_select_db ($myconnection, 'movie') or die ('Could not select database');
+ $mydb = mysqli_select_db ($myconnection, 'db2') or die ('Could not select database');
 
  ?>
