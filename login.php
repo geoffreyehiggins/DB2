2239ae073d81cbcd726703351f50348993cb633b
geoffreyehiggins <geoffrey_higgins@student.uml.edu>
2024-02-26 15:57:00 -0500

    Update Login.php
    update

--- login.php
+++ login.php
@@ -1,11 +1,25 @@
 <?php
 
 $email = $_POST['email'];
-$password = $_POST['password'] 
+$password = $_POST['password'];
  
 $myconnection = mysqli_connect('localhost', 'root', '') 
    or die ('Could not connect: ' . mysql_error());
 
- $mydb = mysqli_select_db ($myconnection, 'db2') or die ('Could not select database');
+$mydb = mysqli_select_db ($myconnection, 'db2') or die ('Could not select database');
 
+$query = 'SELECT email FROM account WHERE email = ' . $email;
+$result = mysqli_query($myconnection, $query) or die("Query Failed: " . mysql_error());
+if($result = NULL) {
+  echo 'No matching email';
+}
+else
+{
+  $query = 'SELECT password FROM account WHERE email = ' . $email;
+  $result = mysqli_query($mydb,$query) or die("Query Failed: " . mysql_error());
+  if($resut != $password)
+  {
+    echo 'Incorrect Password';
+  }
+}
  ?>
