2f5dece4c841337f4586e019a1dc0751f5764b90
Anthony-Terravecchia <70961223+Anthony-Terravecchia@users.noreply.github.com>
2024-03-25 17:20:05 -0400

    abc

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -166,7 +166,8 @@ function createCourseSection() {
                                          WHERE student_id = '$student_id'";
         $existing_advisor_result = mysqli_query($myconnection, $check_existing_advisor_query);
         $existing_advisor_count = mysqli_fetch_assoc($existing_advisor_result)['advisor_count'];
-    
+        
+
         if ($existing_advisor_count == 1 || $existing_advisor_count == 2) {
                 // Student already has maximum advisors
                 // Check if the advisor is already appointed for the student
@@ -175,9 +176,9 @@ function createCourseSection() {
                                             WHERE student_id = '$student_id' 
                                             AND instructor_id = '$advisor_instructor_id'";
             $existing_advisor_result = mysqli_query($myconnection, $check_existing_advisor_query);
-            $existing_advisor_count = mysqli_num_rows($existing_advisor_result);
+            $assigned_advisor_count = mysqli_num_rows($existing_advisor_result);
 
-            if ($existing_advisor_count > 0) {
+            if ($assigned_advisor_count > 0) {
                 // Advisor already appointed for the student, update the record
                 $update_advisor_query = "UPDATE Advise 
                                         SET start_date = '$advisor_start_date', end_date = " . ($advisor_end_date ? "'$advisor_end_date'" : "NULL") . " 
