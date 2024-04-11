469989b5348782e1503596cf91fa5558de03e35f
Anthony-Terravecchia <Anthony_Terravecchia@student.uml.edu>
2024-04-11 00:04:42 -0400

    mobile class reg stub

---
+++ mobile_class_registration.php
@@ -0,0 +1,20 @@
+<?php
+
+$myconnection = mysqli_connect('localhost', 'root', '') or die ('Could not connect: ' . mysqli_error());
+    // $courseId = $_GET['course_id'];
+    $mydb = mysqli_select_db($myconnection, 'db2') or die ('Could not select database');
+    
+    #query to print all currently available courses
+    $query = "SELECT * FROM section";
+    $result = mysqli_query($myconnection, $query) or die("Query Failed: " . mysqli_error($myconnection));
+
+    $data = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+
+    echo json_encode($data);
+
+    mysqli_close($myconnection);
+
+?>
